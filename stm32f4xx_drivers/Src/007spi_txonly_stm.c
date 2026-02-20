d207aca35dadfbe6ecc46de60b54ca3c9b009898
Nurgül ELMAS <nurglelmas@gmail.com>
2026-02-20 14:27:46 +0300

    Update stm32f4xx_drivers folder

--- stm32f4xx_drivers/Src/007spi_txonly_stm.c
+++ stm32f4xx_drivers/Src/007spi_txonly_stm.c
@@ -1,11 +1,18 @@
 /*
- * 006spi_tx_testing.c
+ * 007spi_txonly_stm.c
  *
- *  Created on: 13 Şub 2026
+ *  Created on: 17 Şub 2026
  *      Author: nurgulelmas
  */
+
+
 #include "stm32f407xx.h"
 #include <string.h>
+
+void delay(void)
+{
+	for(uint32_t i=0;i<500000/2;i++);
+}
 
 /*
  * PB14-->SPI2_MISO
@@ -39,8 +46,8 @@ void SPI2_GPIOInits(void)
 	//GPIO_Init(&SPIPins);
 
 	//NSS
-	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
-	//GPIO_Init(&SPIPins);
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&SPIPins);
 }
 void SPI2_Inits(void)
 {
@@ -49,37 +56,77 @@ void SPI2_Inits(void)
 	SPI2handle.pSPIx = SPI2;
 	SPI2handle.SPIConfig.SPI_BusConfig = SPI_BUS_CONFIG_FD;
 	SPI2handle.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
-	SPI2handle.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV16;//generetes sclk of 8Mhz
+	SPI2handle.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV8;//generetes sclk of 2Mhz
 	SPI2handle.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
-	SPI2handle.SPIConfig.SPI_CPOL = SPI_CPOL_HIGH;
+	SPI2handle.SPIConfig.SPI_CPOL = SPI_CPOL_LOW;
 	SPI2handle.SPIConfig.SPI_CPHA =  SPI_CPHA_LOW;
-	SPI2handle.SPIConfig.SPI_SSM = SPI_SSM_EN;//software slave management enabled for NSS pin
+	SPI2handle.SPIConfig.SPI_SSM = SPI_SSM_DI;//hardware slave management enabled for NSS pin
 
 	SPI_Init(&SPI2handle);
 
+}
+void GPIO_ButtonInit(void)
+{
+	GPIO_Handle_t GPIOBtn;
+	//this is btn gpio configuration
+
+		GPIOBtn.pGPIOx = GPIOA;
+		GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+		GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+		GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+		GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl= GPIO_NO_PUPD;
+
+
+		GPIO_Init(&GPIOBtn);
 }
 int main(void)
 {
 
 	char user_data[]= "Hello World";
+
+	GPIO_ButtonInit();
+
+
 	//this function is used initialize the GPIO pins to behave as SPI2 pins
 	SPI2_GPIOInits();
 	//This function is used to initialize the SPI2 peripheral parameters
 	SPI2_Inits();
-	//This makes NSS signal internally high and avoid MODF error
-	SPI_SSIConfig(SPI2, ENABLE);
-	//enabled the SPI peripheral
+
+	/*
+	 * making SSOE 1 does NSS output enable.
+	 * The NSS pin is automatically managed by the hardware.
+	 * i.e. when SPE=1, NSS will be pulled to low
+	 * and NSS pin will be high when SPE=0
+	 */
+	SPI_SSOEConfig(SPI2,ENABLE);
+
+	while(1)
+	{
+	//wait till button is pressed
+	while( ! GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0));
+
+	//to avoid button de-bouncing related issues 200ms of delay
+	delay();
+
+	//enabled the SPI2 peripheral
 	SPI_PeripheralControl(SPI2, ENABLE);
+
+	//first send lenght information
+	uint8_t testData = strlen(user_data);
+	SPI_SendData(SPI2,&testData,1);
+
 	//to send data
 	SPI_SendData(SPI2,(uint8_t*)user_data,strlen(user_data));
 
 	//lets conform SPI not busy
-	while(SPI_GetFlagStatus(SPI2,SPI_BUSY_FLAG));
+	// Wait until SPI is not busy
+	while(SPI_GetFlagStatus(SPI2, SPI_BSY_FLAG));
+	SPI_PeripheralControl(SPI2, DISABLE);
+
 
 	//disabled the SPI2 peripheral
 	SPI_PeripheralControl(SPI2, DISABLE);
-
-	while(1);
+	}
 
 	return 0;
 }
